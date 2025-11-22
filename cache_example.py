3fb3cf0f4869a6340d8400ba1fb60cf90af60e62
Akhilesh <98381403+Akhilesh29@users.noreply.github.com>
2025-11-22 20:57:11 +0530

    minor update

--- cache_example.py
+++ cache_example.py
@@ -2,7 +2,8 @@ import redis
 cache = redis.Redis(host='localhost', port=6379)
 
 # Setting cache
-cache.set('user:1', 'John Doe')
+cache.set('user:1', 'akhilesh')
+
 # Get cache
 user = cache.get('user:1')
 print(user) 
